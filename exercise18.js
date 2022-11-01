dadc54fb2fe6f9fbc835d55ce04bb70c1c9ce3eb
le-toan-1 <85981385+le-toan-1@users.noreply.github.com>
2022-11-02 02:01:40 +0700

    issue fs

--- exercise18.js
+++ exercise18.js
@@ -1,14 +1,31 @@
-const fs = require('fs');
+import * as fs from 'fs';
 
 const validate = (obj) => {
-  if (dataStudents.some((student) => student.id === obj.id)) {
-    console.log("ID must be unique");
-    return false;
-  }
-  if (obj.name.length < 2) {
-    console.log("Name must be at least 2 characters");
-    return false;
-  }
+  if(dataStudents.some(student => student.id === obj.id)) {
+		console.log('ID must be unique !')
+		return false
+	}
+
+	if(obj.name.length < 3) {
+		console.log('Name is too short, at least 3 characters !')
+		return false
+	}
+
+	if(obj.age <= 0) {
+		console.log('Age must be greater than 0');
+		return false
+	}
+
+	if(!['EXCELLENT', 'GOOD', 'AVERAGE'].includes(obj.type)) {
+		console.log('Type must be EXCELLENT, GOOD or AVERAGE')
+		return false
+	}
+
+	if(obj.balance <= 0) {
+		console.log('Balance must be positive')
+		return false
+	}
+	return true
 };
 
 const create = (obj) => {
@@ -30,17 +47,29 @@ const findById = (id) => {
   return dataStudents.find((student) => student.id === id);
 };
 
+const findAll = (type, page, size) => {
+	if(page <= 0 || size <= 0) {
+		console.log('Page and size must be greater than 0');
+		return false
+	}
 
+	const startIndex = page * size - size
+	const paginationStudent = [...dataStudents.slice(startIndex, startIndex + size)]
 
-const dataStudents = JSON.parse(readFileSync("./students.json"));
+	switch (type) {
+		case '':
+			return paginationStudent
+		case 'EXCELLENT':
+			return paginationStudent.filter(student => student.type === 'EXCELLENT');
+		case 'GOOD':
+			return paginationStudent.filter(student => student.type === 'GOOD');
+		case 'AVERAGE':
+			return paginationStudent.filter(student => student.type === 'AVERAGE');
+		default:
+			throw new Error('Invalid' + type)
+	}
+}
 
-create({
-  id: 10,
-  name: "Thanh",
-  age: "28",
-  type: "GOOD",
-  balance: 6867,
-  subjects: ["math", "physic", "chemistry"],
-});
+const dataStudents = JSON.parse(fs.readFileSync("./students.json"));
 
-writeFileSync("./students.json", JSON.stringify(dataStudents));
+fs.writeFileSync("./students.json", JSON.stringify(dataStudents));
