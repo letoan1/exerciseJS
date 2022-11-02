4026db3c3b9f5613f8780420500a5a06b20c0d21
le-toan-1 <85981385+le-toan-1@users.noreply.github.com>
2022-11-02 15:34:24 +0700

    update lastest

--- exercise18.js
+++ exercise18.js
@@ -2,77 +2,93 @@ const fs = require('fs');
 
 const types = ['EXCELLENT', 'GOOD', 'AVERAGE']
 
-const validate = (obj) => {
-    if (dataStudents.some(student => student.id === obj.id)) {
-        console.log('ID must be unique !')
-        return false
-    }
-
-    if (obj.name.length < 3) {
-        console.log('Name is too short, at least 3 characters !')
-        return false
-    }
-
-    if (obj.age <= 0) {
-        console.log('Age must be greater than 0');
-        return false
-    }
-
-    if (!types.includes(obj.type)) {
-        console.log('Type must be EXCELLENT, GOOD or AVERAGE')
-        return false
-    }
-
-    if (obj.balance <= 0) {
-        console.log('Balance must be positive')
-        return false
-    }
-    return true
+const idAlreadyExists = (id) => {
+	return students.some((student) => student.id === id);
 };
 
-const create = (obj) => {
-    if (!validate(obj)) return
-    dataStudents.push(obj)
+const validate = (obj) => {
+	if(obj.name.length < 3) {
+		console.log('Name is too short, at least 3 characters !')
+		return false
+	}
+
+	if(obj.age <= 0) {
+		console.log('Age must be greater than 0');
+		return false
+	}
+
+	if(!types.includes(obj.type)) {
+		console.log('Type must be EXCELLENT, GOOD or AVERAGE')
+		return false
+	}
+
+	if(obj.balance <= 0) {
+		console.log('Balance must be positive')
+		return false
+	}
+	return true
 }
 
-const update = (obj, id) => {
-    if (!validate(obj)) return
-    const studentUpdate = dataStudents[dataStudents.findIndex((student) => student.id === id)] = obj;
-    return studentUpdate
+const create = (obj) => {
+	if(students.some(student => student.id === obj.id)) {
+		console.log('ID must be unique !')
+		return
+	}
+
+	if(!validate(obj)) return
+	students.push(obj)
+}
+
+const update = (id, obj) => {
+	if(!validate(obj)) return
+	if (!idAlreadyExists(id)) {
+		console.log("ID does not exist");
+		return;
+	}
+	const findIndexStudent = students.findIndex((student) => student.id === id)
+	const studentUpdate = students[findIndexStudent] = obj;
+	return studentUpdate
 }
 
 const deleteStudent = (id) => {
-    const removeStudents = dataStudents.filter(student => student.id !== id)
-    return removeStudents
+	if (!idAlreadyExists(id)) {
+		console.log("ID does not exist");
+		return;
+	  }
+	const removeStudents = students.filter(student => student.id !== id)
+	return removeStudents
 }
 
 const findById = (id) => {
-    const findStudents = dataStudents.find((student) => student.id === id);
-    return findStudents
+	if (!idAlreadyExists(id)) {
+		console.log("ID does not exist");
+		return;
+	  }
+	const findStudents = students.find((student) => student.id === id);
+	return findStudents
 };
 
 const findAll = (type, page, size) => {
-    if (page <= 0 || size <= 0) {
-        console.log('Page and size must be greater than 0');
-        return false
-    }
+	if(+page <= 0 || +size <= 0) {
+		console.log('Page and size must be numerical and greater than 0');
+		return false
+	}
 
-    const startIndex = page * size - size
-    const paginationStudent = [...dataStudents.slice(startIndex, startIndex + size)]
+	const startIndex = page * size - size
+	const paginationStudent = [...students.slice(startIndex, startIndex + size)]
 
-    switch (type) {
-        case '':
-            return paginationStudent
-        case 'EXCELLENT':
-            return paginationStudent.filter(student => student.type === 'EXCELLENT');
-        case 'GOOD':
-            return paginationStudent.filter(student => student.type === 'GOOD');
-        case 'AVERAGE':
-            return paginationStudent.filter(student => student.type === 'AVERAGE');
-        default:
-            throw new Error('Invalid' + type)
-    }
-
+	switch (type) {
+		case '':
+			return paginationStudent
+		case 'EXCELLENT':
+			return paginationStudent.filter(student => student.type === 'EXCELLENT');
+		case 'GOOD':
+			return paginationStudent.filter(student => student.type === 'GOOD');
+		case 'AVERAGE':
+			return paginationStudent.filter(student => student.type === 'AVERAGE');
+		default:
+			throw new Error('Invalid' + type)
+	}
 
 }
 
